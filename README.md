9dc047f4138c3c4fbe9d2a8017b8ab18f1cc96ef
vcontrer4s <vikmacon@gmail.com>
2020-05-27 20:07:32 -0400

    pagina web tema oscuro bootstrap

---
+++ README.md
@@ -0,0 +1,2 @@
+# web-dark
+Página web con información del lanzamiento espacial de la nave Crew Dragon, además cuenta con tema oscuro, desarrollada usando caracteristicas de Bootstrap, efectos de jquery y scrollreveal.
---
+++ js/script.js
@@ -0,0 +1,52 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin: 'bottom'
+});
+sr.reveal('.header-content-right', {
+  duration: 2000,
+  origin: 'top',
+  distance: '300px'
+});
+sr.reveal('.header-content-left', {
+  duration: 2000,
+  origin: 'top',
+  distance: '300px'
+});
+sr.reveal('#testimonial', {
+  duration: 2000,
+  origin: 'left',
+  distance: '400px',
+  delay: 1000
+});
+sr.reveal('#contact', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '400px',
+  delay: 1000
+});
+sr.reveal('#cont-izq', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '400px',
+  delay: 1000
+});
+sr.reveal('#cont-der', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '400px',
+  delay: 1000
+});
+
+sr.reveal('.info-right', {
+  duration: 2000,
+  origin: 'right',
+  distance: '400px',
+  delay: 1000
+});
+sr.reveal('.info-left', {
+  duration: 2000,
+  origin: 'left',
+  distance: '400px',
+  delay: 1000
+});
